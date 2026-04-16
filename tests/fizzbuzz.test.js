87e75893b1f2c9637167fd4227bf95245a60525e
Giacomo C. <giacomo.corso@gmail.com>
2026-04-16 10:56:38 +0200

    feat: exercise fizzbuzz ready to start

---
+++ tests/fizzbuzz.test.js
@@ -0,0 +1,18 @@
+const { describe, test } = require("vitest");
+
+
+describe('FizzBuzz', () => {
+    
+    test('should return "Fizz" for multiples of 3', () => {
+    
+    });
+
+    test('should return "Buzz" for multiples of 5', () => {
+
+    });
+
+    test('should return "FizzBuzz" for multiples of both 3 and 5', () => {
+    
+    });
+
+})
