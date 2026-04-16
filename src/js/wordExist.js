c8377f61f1270742e6f0314c381fa72478cd1dcf
Giacomo C. <giacomo.corso@gmail.com>
2026-04-16 10:37:05 +0200

    review: exercise - wordExist

--- src/js/wordExist.js
+++ src/js/wordExist.js
@@ -1,4 +1,13 @@
+// saber si la frase contiene la palabra English
 
-export function wordExist() {
-    
+// con una frase dada sin tener en cuenta mayusculas o minusculas
+// devuelve true
+
+// la frase = string ?
+// la palabra = string ? como le paso la palabra a buscar y la frase a la function?
+// normalizar a miniscula la frase y la palabra ? si o no lo se
+// comprobar si la palabra existe en la frase ? ????
+
+export function wordExist(sentence, word) {
+    return sentence.toLowerCase().includes(word.toLowerCase());
 }
