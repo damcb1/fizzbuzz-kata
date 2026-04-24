32fad2236101526f2ce1a7ce49db469cadf4105f
Damaris Castro <damariscastro@MacBook-Air-de-Damaris.local>
2026-04-24 10:46:24 +0200

    Complete FizzBuzz kata with tests

--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -1,18 +1,18 @@
-const { describe, test } = require("vitest");
-
+import { describe, test, expect } from "vitest";
+import FizzBuzz from "../src/js/fizzbuzz";
 
 describe('FizzBuzz', () => {
     
     test('should return "Fizz" for multiples of 3', () => {
-    
+        expect(FizzBuzz(3)) .toBe ("Fizz");    
     });
 
     test('should return "Buzz" for multiples of 5', () => {
-
+        expect(FizzBuzz(5)) .toBe("Buzz");
     });
 
     test('should return "FizzBuzz" for multiples of both 3 and 5', () => {
-    
+        expect(FizzBuzz(15)) .toBe("FizzBuzz");    
     });
 
-})
+});
